0a8b786d51631657a51009a6ca431c807ef8d438
hakunozen <sirjeromepatiga@gmail.com>
2019-01-09 17:32:08 +0800

    add chat header

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="Home">
-    
+  <div class="Home chat">
+    <div class="chat__header">
+      <p>@ Kiana</p>
+      <div class="status active"/>
+    </div>
+    <div class="chat__body">
+      <div class="chat__start">
+      </div>
+      <ul>
+        <li>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
-}
+  name: "Home"
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
