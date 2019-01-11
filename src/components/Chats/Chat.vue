8272720c285d56ed735e6ee2a841f1752bd0a747
hakunozen <sirjeromepatiga@gmail.com>
2019-01-11 09:50:40 +0800

    changed static chat messages to dynamic

---
+++ src/components/Chats/Chat.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="chat__body-incoming">
+    <!-- <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxyCupEcex8TQ972NCU17qPgMAJEsMt8c2ffXQVwytX2j_Gkjs"
+    > -->
+    <img :src="chat.avatar"/>
+    <div class="chat__body-text">
+      <div class="chat__body-text-info">
+        <p class="chat__body-text-info-name">{{chat.name}}</p>
+        <p class="chat__body-text-info-date">{{chat.date}}</p>
+      </div>
+      <p class="chat__body-text-message">{{chat.msg}}</p>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: 
+  {
+    chat: Object
+  }
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,72 +12,7 @@
         <p>It all started here ~</p>
       </div>
       <ul>
-        <li class="chat__body-incoming">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxyCupEcex8TQ972NCU17qPgMAJEsMt8c2ffXQVwytX2j_Gkjs"/>
-          <div class="chat__body-text">
-            <div class="chat__body-text-info">
-              <p class="chat__body-text-info-name">Kiana</p>
-              <p class="chat__body-text-info-date">Today at 1:57PM</p>
-            </div>
-            <p class="chat__body-text-message">Hi there Captain ~ !</p>
-          </div>
-        </li>
-        <li class="chat__body-outgoing">
-          <img src="https://pm1.narvii.com/6991/444e4ab9d55132405af1b5dbac03ed6341f1f245r1-641-429v2_hq.jpg"/>
-          <div class="chat__body-text">
-            <div class="chat__body-text-info">
-              <p class="chat__body-text-info-name">Captain</p>
-              <p class="chat__body-text-info-date">Today at 1:57PM</p>
-            </div>
-            <p class="chat__body-text-message">No..</p>
-          </div>
-        </li>
-        <li class="chat__body-incoming">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxyCupEcex8TQ972NCU17qPgMAJEsMt8c2ffXQVwytX2j_Gkjs"/>
-          <div class="chat__body-text">
-            <div class="chat__body-text-info">
-              <p class="chat__body-text-info-name">Kiana</p>
-              <p class="chat__body-text-info-date">Today at 1:57PM</p>
-            </div>
-            <p class="chat__body-text-message">But, i didn't even said anything yet</p>
-            <p class="chat__body-text-message">D:</p>
-          </div>
-        </li>
-        <li class="chat__body-outgoing">
-          <img src="https://pm1.narvii.com/6991/444e4ab9d55132405af1b5dbac03ed6341f1f245r1-641-429v2_hq.jpg"/>
-          <div class="chat__body-text">
-            <div class="chat__body-text-info">
-              <p class="chat__body-text-info-name">Captain</p>
-              <p class="chat__body-text-info-date">Today at 1:57PM</p>
-            </div>
-            <p class="chat__body-text-message">No..</p>
-            <p class="chat__body-text-message">I know what you want Kiana...</p>
-            <p class="chat__body-text-message">And I'm not having any of it.</p>
-          </div>
-        </li>
-        <li class="chat__body-incoming">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxyCupEcex8TQ972NCU17qPgMAJEsMt8c2ffXQVwytX2j_Gkjs"/>
-          <div class="chat__body-text">
-            <div class="chat__body-text-info">
-              <p class="chat__body-text-info-name">Kiana</p>
-              <p class="chat__body-text-info-date">Today at 1:57PM</p>
-            </div>
-            <p class="chat__body-text-message">But, Kaaaaannnnccchhhhhoooooooouuuuuuu D:</p>
-            <p class="chat__body-text-message">-play sad piano</p>
-          </div>
-        </li>
-        <li class="chat__body-bot">
-          <img src="https://images.discordapp.net/avatars/434737143395516416/a2b9761b4fd5af86af088200c6060b94.png?size=512"/>
-          <div class="chat__body-text">
-            <div class="chat__body-text-info">
-              <p class="chat__body-text-info-name">Ai-chan</p>
-              <p class="chat__body-text-info-date">Today at 1:57PM</p>
-            </div>
-            <p class="chat__body-text-message">please type the number from these query results</p>
-          <div class="chat__body-bot-card">
-            </div>
-          </div>
-        </li>
+        <chat v-for="(chat, index) in messages" :key="index" :chat="chat"/>
       </ul>
     </div>
     <div class="chat__input">
@@ -87,8 +22,44 @@
 </template>
 
 <script>
+import Chat from './Chats/Chat.vue';
 export default {
-  name: "Home"
+
+  name: "Home",
+  components: {
+    Chat
+  },
+  data() {
+    return {
+      messages: [
+        {
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxyCupEcex8TQ972NCU17qPgMAJEsMt8c2ffXQVwytX2j_Gkjs',
+          name: 'Kiana',
+          msg: 'Hi captain',
+          date: 'Today :7:04',
+        },
+        {
+          avatar: 'https://pm1.narvii.com/6991/444e4ab9d55132405af1b5dbac03ed6341f1f245r1-641-429v2_hq.jpg',
+          name: 'Captain',
+          msg: 'No.',
+          date: 'Today :7:04',
+        },
+        {
+          avatar: 'https://images.discordapp.net/avatars/434737143395516416/a2b9761b4fd5af86af088200c6060b94.png?size=512',
+          name: 'Ai-Chan',
+          msg: 'HIiiiiiiiiiiiiiiiiiiiii!!!!',
+          date: 'Today :7:05',
+        },
+        {
+          avatar: 'https://images.discordapp.net/avatars/434737143395516416/a2b9761b4fd5af86af088200c6060b94.png?size=512',
+          name: 'Ai-Chan',
+          msg: 'You guys wanna roll on the new gatcha ?',
+          date: 'Today :7:05',
+        },
+
+      ]
+    }
+  }
 };
 </script>
 
