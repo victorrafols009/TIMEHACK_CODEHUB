a524dc6c8d8075cd91dfbce15cafea6d10c91a3b
vhid91 <vhid.abaiz@gmail.com>
2019-01-14 19:37:38 +0800

    initial integration of watson

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
 var Logger  = require('./modules/common/services/Logger');
 // var Errors  = require('./modules/common/services/Errors');
 // var path    = require('path')
 var express = require('express');
-var app     = express();
 
-require('dotenv').config();
+var bodyParser = require('body-parser');
+
+var app     = express();
+var ask     = require('./modules/common/services/controller').ask;
 
 var express_configuration = require("./express-configuration");
 express_configuration.init(app, express);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1/messages', require('./modules/message'));
+app.post('/ask', ask)
 
 var server = require('http').createServer(app);
 var io     = require('socket.io')(server);
@@ -31,6 +37,20 @@ io.on('connection', (socket) => {
     io.emit('connections', connections);
     Logger.log('info', '[Socket] User disconnected', {connections: connections});
   })
+});
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+  );
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type'
+  );
+  next();
 });
 
 module.exports = app;
